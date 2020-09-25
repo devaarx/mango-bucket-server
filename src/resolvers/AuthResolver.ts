1cf7dd5782054b76b51842f6b8af6d9ef3c15159
iamdebadipti <iamdebadipti@gmail.com>
2020-09-25 20:14:50 +0530

    🔑 sign in mutation added

--- src/resolvers/AuthResolver.ts
+++ src/resolvers/AuthResolver.ts
@@ -1,6 +1,9 @@
+import { compare, hash } from 'bcryptjs';
 import { validate } from 'class-validator';
-import { Args, ArgsType, Field, Mutation, Query, Resolver } from 'type-graphql';
+import { sign } from 'jsonwebtoken';
+import { Args, ArgsType, Ctx, Field, Mutation, Query, Resolver } from 'type-graphql';
 import { User } from '../entity/User';
+import { Context } from '../interfaces/context';
 
 // register arg types
 @ArgsType()
@@ -8,6 +11,16 @@ class RegisterArgs {
   @Field()
   name: string;
 
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+// login arg types
+@ArgsType()
+class LoginArgs {
   @Field()
   email: string;
 
@@ -31,11 +44,12 @@ export class AuthResolver {
     if (duplicate) {
       throw Error('user exists');
     } else {
-      // create new user entity instance
-      let user = new User();
+      let user = new User(); // create new user entity instance
+      const hashedPassword = await hash(password, 12); // hash password
+
       user.name = name;
       user.email = email;
-      user.password = password;
+      user.password = hashedPassword;
 
       const errors = await validate(user); // validate the user
 
@@ -48,5 +62,25 @@ export class AuthResolver {
 
       return user;
     }
+  }
+
+  @Mutation(() => User)
+  async login(@Args() { email, password }: LoginArgs, @Ctx() { res }: Context) {
+    const user = await User.findOne({ email }); // check for user
+
+    if (!user) {
+      throw Error('no user found');
+    }
+
+    const validPassword = await compare(password, user.password); // check the password
+
+    if (!validPassword) {
+      throw Error('invalid password');
+    }
+
+    const signedId = await sign({ user_id: user.id }, process.env.JWT_SECRET_TOKEN, { expiresIn: '1d' }); // jwt signed userid
+    res.cookie('jwt', signedId, { httpOnly: true, maxAge: 3600000 * 24 }); // set cookie
+
+    return user;
   }
 }
